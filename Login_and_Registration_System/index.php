b40b42ac0e19d8b59f56fcea3b9f7cdf47e36e93
Harun <harunselcuk@mail.com>
2025-03-20 12:40:27 +0100

    added folders and started first project, loginregister

---
+++ Login_and_Registration_System/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <a href=""><img src="" alt=""></a>
+        <form action="app.php" method="post">
+            <input type="button" value="Login">
+            <input type="button" value="Sign Up">
+        </form>
+    </header>
+    <main>
+        <section>
+            <div>
+                <div>
+                    <form action="app.php" method="post">
+                        <input type="text" name="username" placeholder="Username">
+                        <input type="text" name="Email" placeholder="Example@email.com">
+                        <input type="password" name="Password" id="Password">
+                    </form>
+                </div>
+            </div>
+            <div>
+                <div>
+
+                </div>
+            </div>
+        </section>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
